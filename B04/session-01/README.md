e1fb858a43b1cedefff8d6a8de449dd6e116882f
hashaaan <shalithadev@gmail.com>
2025-08-30 17:32:57 +0530

    Add B04 session 01 practical code

---
+++ B04/session-01/README.md
@@ -0,0 +1,34 @@
+# React.js and Next.js Mastering Course (Batch 04)
+
+This session is part of the **React.js and Next.js Mastering Course** and focuses on foundational concepts essential for modern web development. You'll learn about popular styling frameworks, the basics of version control systems, core JavaScript concepts (including the event loop and ECMAScript versions), and practical Git commands for managing your codebase. By the end of this session, you'll have a solid understanding of how to style applications, manage code versions, and work with JavaScript fundamentals—skills that are crucial for any React or Next.js developer.
+
+## Session 01
+
+### 1. Initial Project Setup
+
+- Introduction to React.js and Next.js
+- Setting up the development environment
+- Creating the first project
+
+### 2. HTML Fundamentals
+
+- Understanding basic HTML structure
+- Commonly used HTML tags
+- Importance and usage of meta tags
+
+### 3. CSS Fundamentals
+
+- Applying inline, embedded, and external CSS
+- Using ID and class selectors for styling
+- Structuring layouts with parent and child divs
+
+### 4. Basic JavaScript Concepts
+
+- Linking and using external JavaScript files
+- Overview of JavaScript's role in web development
+- Simple examples and best practices
+
+## References
+
+- [flatcolors.com](https://flatuicolors.com/)
+- Add a new change here in README.md
---
+++ B04/session-01/script.js
@@ -0,0 +1,67 @@
+// External JavaScript file
+
+// JavaScript Variable Types
+
+// Var Type
+// JavaScript var Hoisting
+// console.log("1::", userEmail); // No Hoisting
+// var userEmail = "shalithadev@gmail.com";
+// console.log("2::", userEmail);
+
+// Let Type
+// console.log("1::", userAge);
+// Declare and Initialize user age as a let variable
+// let userAge; // Init // Number
+// userAge = 28; // Assign // Number
+// console.log("2::", userAge);
+// // Update the already declared let variable
+// userAge = 29;
+// console.log("3::", userAge);
+
+// Const Type
+// console.log("1::", userName); // No Hoisting
+// const userName = "Hashan Shalitha"; // String
+// userName = "Hashan Shalitha"; // String
+// console.log("2::", userName);
+// userName = "Ravindu"; // No Re-assignments
+// console.log("3::", userName);
+
+
+const isAuthenticated = false; // Boolean
+const lastName = null;
+const firstName = undefined;
+const userData = {
+  firstName: "Hashan",
+  lastName: "Shalitha",
+  age: 29,
+};
+// Arrays
+const fruits = ["banana", "apple", "guava"]; // String Array
+const numbers = [1, 2, 3, 4]; // Number Array
+const users = [
+  { id: 1, name: "Hashan" },
+  { id: 2, name: "Shalitha" },
+  { id: 3, name: "Kavindu" },
+]; // Object Array
+
+// userName = "Shalitha Hashan"
+
+// This is a single line comment
+
+/*
+This is a
+multi-line
+comment
+*/
+
+// console.log("I'm second javascript file here!", {userEmail, userName, userAge});
+
+// console.log(userEmail, typeof userEmail);
+// console.log(userAge, typeof userAge);
+// console.log("value:", isAuthenticated, "type:", typeof isAuthenticated);
+// console.log("value:", lastName, "type:", typeof lastName);
+// console.log("value:", firstName, "type:", typeof firstName);
+// console.log("value:", userData, "type:", typeof userData);
+// console.log("value:", fruits, "type:", typeof fruits);
+// console.log("value:", numbers, "type:", typeof numbers);
+// console.log("value:", users, "type:", typeof users);
